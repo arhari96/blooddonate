6e2b4d05e7f957fa80a073d562c089167f0bf08c
arhari96 <haribaburamakrishnan@gmail.com>
2024-03-13 16:37:53 +0530

    app added

--- blooddonateproject/blooddonateproject/settings.py
+++ blooddonateproject/blooddonateproject/settings.py
@@ -31,12 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blooddonateapp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+     "allauth.socialaccount",
+    "allauth.socialaccount.providers.google",
 ]
 
 MIDDLEWARE = [
